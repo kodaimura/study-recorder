f1fc2cd856ccf69d8907c593ad1eb8566caf6f13
kodaimura <kodaimura123@gmail.com>
2024-05-24 18:25:43 +0900

    add HttpError

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -1,26 +1,40 @@
-export const BASE_URL = 'api';
+class HttpError extends Error{
+    public status: number;
+
+    constructor(status: number, message: string) {
+        super(message);
+        this.status = status;
+    }
+}
 
 class Api {
+    public url: string;
+
+    constructor(url: string) {
+        this.url = url;
+    }
+    
+    /**
+     * @throws { HttpError } If an API error occurs.
+    */
     private apiFetch = async (endpoint: string, method: string, body: any): Promise<any> => {
         try {
             let header: any = {
                 method: method,
-                  headers: {
+                headers: {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${localStorage.token}`
-                  },
+                },
             };
     
             if (body) {
                 header.body = JSON.stringify(body);
             }
-            const response = await fetch(`${BASE_URL}/${endpoint}`, header);
+            const response = await fetch(`${this.url}/${endpoint}`, header);
     
             if (!response.ok) {
                 const errorData = await response.json();
-                const error = new Error(errorData.message || 'An error occurred') as any;
-                error.status = response.status;
-                throw error;
+                throw new HttpError(response.status, errorData.message);
             }
     
             let data;
@@ -28,9 +42,7 @@ class Api {
                 data = await response.json();
             } catch (error) {
                 if (response.status !== 204 && response.status !== 200) {
-                    const error = new Error('Error parsing JSON') as any;
-                    error.status = response.status;
-                    throw error;
+                    throw new HttpError(response.status, 'Error parsing JSON');
                 }
             }
             return data;
@@ -43,21 +55,23 @@ class Api {
         }
     };
 
-    public get = async (endpoint: string) => {
+    public get = async (endpoint: string): Promise<any> => {
         return this.apiFetch(endpoint, 'GET', null);
     };
     
-    public post = async (endpoint: string, body: any) => {
+    public post = async (endpoint: string, body: any): Promise<any> => {
         return this.apiFetch(endpoint, 'POST', body);
     };
     
-    public put = async (endpoint: string, body: any) => {
+    public put = async (endpoint: string, body: any): Promise<any> => {
         return this.apiFetch(endpoint, 'PUT', body);
     };
     
-    public delete = async (endpoint: string) => {
+    public delete = async (endpoint: string): Promise<any> => {
         return this.apiFetch(endpoint, 'DELETE', null);
     };
 }
 
-export const api = new Api();
+export const BASE_URL = 'api';
+
+export const api = new Api(BASE_URL);
